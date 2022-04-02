5caa205b35f00185d3bb4a186c6d43fff9e2aefc
unknown <islam15-3879@diu.edu.bd>
2022-04-02 23:22:19 +0600

    css-added

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import useCart from '../../hooks/useCart';
 import useProducts from '../../hooks/useProducts';
+import Cart from '../Cart/Cart';
+import Reviewitem from '../ReveiItem/Reviewitem';
 
 const Order = () => {
 
     const [products,setProducts]=useProducts();
     const [cart,setCart]=useCart(products);
     return (
-        <div>
-            <h3>this is order vai</h3>
-            <h2>this is  a products{products.length}</h2>
-            <p>Cart has {cart.length}</p>
+        <div className='shop-container' >
+            <div className='review-item-container' >
+                 {
+                  cart.map(product=><Reviewitem
+                  key={product.id}
+                  product={product}
+                  
+                  ></Reviewitem>)
+                } 
+
+            </div>
+            <div className="cart-container">
+                <Cart cart={cart} ></Cart>
+            </div>
+           
         </div>
     );
 };
---
+++ src/components/ReveiItem/Reviewitem.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Reviewitem.css';
+const Reviewitem = (props) => {
+    const{name,img,price,shipping,quantity}=props.product;
+    return (
+        <div className='reveiw-item' >
+            <div>
+                <img src={img} alt="" />
+            </div>
+            <div className="review-item-details-container">
+
+                <div className="review-item-details">
+
+                <p className="product-name" title={name}>{name.length >20? name.slice(0,20)+'...' : name}</p>
+                <p>Price:<span className='orange-color'>{price}</span></p>
+                <p><small>Shipping:{shipping}</small></p>
+                <p><small>Quantity:{quantity}</small></p>
+                </div>
+
+                <div className="delete-container">
+                    <button>delete</button>
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Reviewitem;
