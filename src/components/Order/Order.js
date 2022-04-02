f23d026c9f92ce05ea51ec42362b6a90959a459e
unknown <islam15-3879@diu.edu.bd>
2022-04-03 00:37:18 +0600

    added

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import useCart from '../../hooks/useCart';
 import useProducts from '../../hooks/useProducts';
 import { removeFromDb } from '../../utilities/fakedb';
@@ -28,7 +29,10 @@ const Order = () => {
 
             </div>
             <div className="cart-container">
-                <Cart cart={cart} ></Cart>
+                <Cart cart={cart} >
+
+                    <Link to='/inventory'><button>Procced cheakout</button></Link>
+                </Cart>
             </div>
            
         </div>
